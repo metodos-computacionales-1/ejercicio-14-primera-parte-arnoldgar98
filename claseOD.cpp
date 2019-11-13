4185d7a1b977e01f997c421f8517bde3744100fe
Default user <arnoldsgg@gmail.com>
2019-11-13 18:30:25 +0000

    arreglado

--- claseOD.cpp
+++ claseOD.cpp
@@ -5,7 +5,7 @@ double f0(double t, double v0, double x0); //Returns derivative of y0
 double f1(double t, double v0, double x0); //Returns derivative of y1
 
 const double K = 100;
-const double M = 2;
+const double M = 50;
 const double LAMBDA = 1;
 const double DeltaT = 0.01;
 
@@ -16,34 +16,33 @@ double f0(double t, double v0, double x0)
 
 double f1(double t, double v0, double x0)
 {
-  return (-K*x0/M);
+  return (-K/M*pow(x0,LAMBDA));
 }
 
 double euler()
 {
     //1000 iteraciones computacionales 
-    int puntos = (1000/0.01);
+    int puntos = (100/0.01);
     float x[puntos];
     float v[puntos];
     float t[puntos];
     x[0]=1;
-    v[0]=0;
+    v[0]=1;
     for(int i=1; i<puntos;i++)
     { 
-        v[i]=v[i-1]+DeltaT*f0(t[i-1],v[i-1],x[i-1]);
-        x[i]=x[i-1]+DeltaT*f1(t[i-1],v[i-1],x[i-1]);
+        v[i]=v[i-1]+DeltaT*f1(t[i-1],v[i-1],x[i-1]);
+        x[i]=x[i-1]+DeltaT*f0(t[i-1],v[i-1],x[i-1]);
+        t[i]=i;
         
     }
     for(int i=0;i<puntos;i++)
     {
-        cout<<x[i]<<";"<<v[i]<<endl;
+        cout<<t[i]<<" "<<v[i]<<" "<<x[i]<<endl;
     }  
     
 }
 int main(void)
 {
     euler();
-    cout<<"se esperan soluciones del tipo seno y coseno"<<endl;
-    cout<<"al iteral 1000 veces se obtiene un error bastante grande respecto a los senos y cosenos"<<endl;
     return 0;
 }
